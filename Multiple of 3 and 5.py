5b880b853ca8e5060443b6e127517bac826e51f2
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2019-11-07 11:07:46 +0530

    Update Multiple of 3 and 5.py

--- Multiple of 3 and 5.py
+++ Multiple of 3 and 5.py
@@ -1,13 +1,12 @@
 #multiple of 3 or 5 and add the numbers
+import time
+start = time.time()
 
-#ask for the maximum number upto which condition is to be checked
-num = int(input("enter the number: "))
 num_sum = 0 #for storing the addition of numbers
-
-for x in range(num):
+for x in xrange(1000):
     if (x%3 == 0) or (x%5 == 0):
         num_sum += x
-    else:
-        pass
 
 print("The total sum is: {}".format(num_sum))
+end = time.time()
+print(end-start)
