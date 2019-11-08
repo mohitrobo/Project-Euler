6e1e3c723799dce79a30dd5fe0c762d726acc027
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2019-11-08 09:48:12 +0530

    Update palindrome product.py

--- palindrome product.py
+++ palindrome product.py
@@ -1,26 +1,19 @@
 #Largest palindrome made from the product of two 3 digit numbers
 
-#store the palindrome values in an empty list
-palin_list = []
+import time
+start = time.time()
+#variable for palindrome product
+palin_num = 0
 
 #multiplication of two 3 digit values in variable a and b
-for a in range(100,1000):
-    for b in range(100,1000):
+for a in xrange(100,1000):
+    for b in xrange(100,1000):
         num = str(a*b)  #convert multiplication into string so as to access the index value for comparison among index
-        num_length = len(num)
-        i = 0 #for index
-        j = num_length - 1 #for index
+        if (num==num[::-1]):
+            palin_num = max(palin_num,int(num))
 
-        #comparison happens here
-        while (num[i] == num[j]):
-            i += 1 #incrementing the index from start
-            j -= 1 #decrementing the index from last
-
-            #check whether middle index has been reached
-            if i == round(num_length / 2):
-                palin_list.append(int(num)) #value is converted back to int
-                break
-
-print(max(palin_list))
+end = time.time()
+print(palin_num)
+print(end-start)
 
         
