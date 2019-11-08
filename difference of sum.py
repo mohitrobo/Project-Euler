28ceaeace77f41c944418c3f107cab172f0f2d4b
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2019-11-08 11:36:27 +0530

    Update difference of sum.py

--- difference of sum.py
+++ difference of sum.py
@@ -1,5 +1,7 @@
 #Difference of the sum of square and square of sum of first 100 numbers
 
+import time
+start = time.time()
 #variable to store sum of square
 sum_square = 0
 #variable to store sum of numbers
@@ -9,8 +11,8 @@ for x in range(1,101):
     sum_square += x**2
     sum_num += x
 
-
+end = time.time()
 print("sum of square: {}".format(sum_square))
 print("square of sum of number: {}".format(sum_num**2))
 print("Difference of the square of sum and sum of square: {}".format(sum_num**2 - sum_square))
-
+print(end-start)
