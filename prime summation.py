887039a281e3e438238046139c8df4125dc64304
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2019-11-08 13:49:04 +0530

    Update prime summation.py

--- prime summation.py
+++ prime summation.py
@@ -3,26 +3,22 @@
 import time
 import math
 
+start = time.time()
+
 #function to find prime numbers
 def prime(num):
-    #prime_store = []
-    sum_prime = 0
-    lower = 2
-    for x in xrange(lower, num+1):
+    sum_prime = 2
+    lower = 3
+    for x in xrange(lower, num+1,2):
         for i in xrange(2,int(math.sqrt(x))+1):
             if (x%i) == 0:
                 break
         else:
-            #prime_store.append(x)
             sum_prime += x
 
-    #return the list
+    #return the sum
     return sum_prime
 
-#ask for maximum value upto which prime number is to be found
-prime_input = int(input("Enter the maximum value upto which prime number is to be found: "))
-prime_summation = prime(prime_input)
-#print(prime_list)
+prime_summation = prime(2000000)
+end = time.time()
 print("The summation of prime numbers: {}".format(prime_summation))
-
-
