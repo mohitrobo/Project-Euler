76e8eba159721599f500ab11e4777deed2942ff4
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2019-11-11 16:27:31 +0530

    Update lattice path.py

--- lattice path.py
+++ lattice path.py
@@ -1,6 +1,7 @@
 #Lattice path
-
+import time
 import math
+start = time.time()
 
 #function to calculate lattice path for a square matrix
 def lattice(n): #nxn matrix
@@ -8,6 +9,7 @@ def lattice(n): #nxn matrix
     return math.factorial(2*n) / (n_fact * n_fact)
 
 
-path = int(input("Enter the size of square matrix(n x n),provide only n: "))
-print(lattice(path))
+print(lattice(20))
+end = time.time()
+print(end-start)
 
