21299d4e28b389dafd4fcbd3738e6e8702dae35d
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2019-11-12 18:18:10 +0530

    Update collatz sequence.py

--- collatz sequence.py
+++ collatz sequence.py
@@ -1,31 +1,41 @@
 #Longest Collatz sequence
 
-#function to find collatz sequence
+import time
+start = time.time()
+
+#function for collatz sequence
 def collatz(num):
-    #while loop check
+    #variable for length of sequence
+    length = 0
+    #check if even or odd and continue the loop until the number=1
     check = True
-
-    #empty list to store sequence
-    coll_seq = [num]
-    
     while check:
-        if num%2==0:  #check even number
+        if num%2 == 0: #even
             num = num/2
-            coll_seq.append(num)
+            length += 1
             if num==1:
                 check = False
-                return coll_seq
-        else:  #odd number
-            num = 3*num + 1
-            coll_seq.append(num)
+        else: #odd
+            num = 3*num+1
+            length += 1
             if num==1:
                 check = False
-                return coll_seq
+
+    return length
+
+#variable for length of sequence for comparison
+compare = 0
+#variable for storing the number and the count
+coll,coll_len = 0,0
+#loop for number
+for number in xrange(13,1000000):
+    count = collatz(number)
+    if count>compare:
+        compare = count
+        coll,coll_len = number,count
 
 
-
-
-
-number = int(input("Enter the number for which collatz sequence is to be found: "))
-sequence = collatz(number)
-print(sequence)
+end = time.time()
+print(coll,coll_len)
+print(end-start)
+        
