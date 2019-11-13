afe45fbfe021b55a0ab26db50acd48a936945653
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2019-11-13 09:04:29 +0530

    Update maximum path sum.py

--- maximum path sum.py
+++ maximum path sum.py
@@ -1,18 +1,32 @@
 #maximum path sum
+import time
+start = time.time()
 
-import numpy as np
+s = '''75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23'''
 
-x = np.array([[3],[7,4],[2,4,6],[8,5,9,3]])
+y = s.split('\n')
+b = y[len(y)-1].split(' ') #for last row
+for i in xrange(len(y),1,-1): #for second last row
+    c = [] #for storing the new row 
+    a = y[i-2].split(' ')
+    for j in xrange(0,len(a)):
+        c.append(int(a[j]) + max(int(b[j]),int(b[j+1])))
+    b = c
 
-
-#i=rows and j=columns
-
-for i in range(len(x) - 2, -1, -1): #bottom to top
-    for j in range(0,i+1): #within the same row column is changed
-        if x[i+1][j] > x[i+1][j+1]:
-            x[i][j] += x[i+1][j]
-        else:
-            x[i][j] += x[i+1][j+1]
-
-print(x[0][0])
-        
+end = time.time()
+print(b)
+print(end-start)
