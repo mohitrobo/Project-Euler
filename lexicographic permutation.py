939fd47dfef5d29b831da190ad563b6845c594a5
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2019-11-20 10:50:06 +0530

    Update lexicographic permutation.py

--- lexicographic permutation.py
+++ lexicographic permutation.py
@@ -1,21 +1,11 @@
-#there is still a confusion to find permutation
+#Lexicographic permutation
 #itertools is used 
 
-
-
-#from math import factorial
-#def perm(n, s):
- #  if len(s)==1: return s
-  # q, r = divmod(n, factorial(len(s)-1))
-   #return s[q] + perm(r, s[:q] + s[q+1:])
-
-#L = 19
-#print "Project Euler 24 Solution = ", perm(L-1, '4321')
-
-
-
-
-#another set of program
+import time
 from itertools import permutations
-x = list(permutations("1234"))
-print(x)
+start = time.time()
+
+x = list(permutations("0123456789"))
+print(''.join(x[999999]))
+end = time.time()
+print(end-start)
