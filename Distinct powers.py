ae47a9c9f60e48ff382e0489016637b09e2f393e
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2019-11-21 11:36:21 +0530

    Update Distinct powers.py

--- Distinct powers.py
+++ Distinct powers.py
@@ -5,15 +5,19 @@
 #sort the list
 #make it a set
 
+import time
+start = time.time()
 
 distinct_power = []
 
-for a in range(2,6):
-    for b in range(2,6):
+for a in range(2,101):
+    for b in range(2,101):
         distinct_power.append(pow(a,b))
 
 
 #create set of unique elements and sort it
 list_sort = sorted(set(distinct_power))
 
-print("The list of values for a^b: {}".format(list_sort))
+print("The list of values for a^b: {}".format(len(list_sort)))
+end = time.time()
+print(end-start)
