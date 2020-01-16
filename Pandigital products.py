2d0352c66044dacb4796c1213c54ec1e2a30ec96
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2020-01-16 20:54:32 +0530

    Update Pandigital products.py

--- Pandigital products.py
+++ Pandigital products.py
@@ -1,34 +1,34 @@
 #Pandigital products
-
-#taking range of a = 1 to 200 and b = 1 to 200
-#multiply a and b
-#store product in p
-#make them as string and concatenate
-#convert them in set and find the length
-# 0 not allowed to be counted
-
 import time
-
 start = time.time()
 
-#empty string to store a,b and p as a list
-store = []
+#for storing the product sum
+products = set()
 
-for a in xrange(1,200):
-    for b in range(1,200):
-        p = a*b
+#Digits to be checked
+digit = set('123456789')
 
-        #string conversion and concatenate and convert into set
-        s = set(str(a)+str(b)+str(p))
-
-        #check if 0 is present in set
-        if '0' in s:
-            pass
-        else:
-            if len(s) == 9:
-                store.append([a,b,p])
-            else:
-                pass
+#for single digit multiplicand
+for a in xrange(1,10):  # a = 1 to 9
+    for b in xrange(999,9999):
+        s = str(a)+str(b)+str(a*b)
+        if set(s) == digit:
+            products.add(a*b)
+        elif len(s)>9:
+            break
 
 
-print(store)
+#for double digit multiplicand
+for a in xrange(10,99):  # a=10 to 99
+    for b in xrange(99,999):
+        s = str(a)+str(b)+str(a*b)
+        if set(s) == digit:
+            products.add(a*b)
+        elif len(s)>9:
+            break
+
+
+print(sum(products))
+
+end = time.time()
+print(end-start)
