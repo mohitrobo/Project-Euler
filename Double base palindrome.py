5b07e27050e77febc566ea7d138b6f381c538424
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2020-02-28 07:11:30 +0530

    Update Double base palindrome.py

--- Double base palindrome.py
+++ Double base palindrome.py
@@ -1,33 +1,27 @@
-#Double-base palindromes check for first 1000 numbers
+#Double-base palindromes check for first 1 million numbers
 #use slicing to check palindrome
 
 import time
 
-start = time.time()
+start = time.time() 
+           
+#store palindrome values
+#palin_list = []
 
-#function for checking palindrome
-def palindrome(num):
+#sum to store the sum of palindrome numbers
+sum_palin = 0
+
+for num in xrange(1,1000000):
     x = str(num) #convert it into string
 
     if x == x[::-1]: #check for reverse of number
         deci_bin = bin(num)
         if deci_bin[2:] == deci_bin[2:][::-1]:
-            palin_list.append([num,deci_bin[2:]])
+            sum_palin += num
 
-        
-
-            
-#store palindrome values
-palin_list = []
-
-for p in xrange(11,1000):
-    palindrome(p)
 
 
 end = time.time()        
-print(palin_list)
+#print(palin_list)
+print(sum_palin)
 print(end - start)
-
-
-
-            
